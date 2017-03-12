672a0f4e09ecff8b6d7b826d5c38a49e586cfe1e
Zhang Yun <sigmarising@hotmail.com>
2017-03-12 10:41:38 +0800

    第一版本的框架完成

---
+++ head/Global_define.h
@@ -0,0 +1,16 @@
+/*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	Name:		Books.h
+	Author:		Zhang Yun
+	Version:	alpha 0.1
+	Intro:		the global variable 
+				and	define 
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
+
+#pragma once
+
+extern long Day;
+extern long Num_stu;
+extern long Num_teach;
+extern long Num_book0;
+extern long Num_book1;
+extern long Num_book2;
